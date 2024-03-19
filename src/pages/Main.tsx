5a3e4e164d8633cdd937d1023fb5191e37cd5cc8
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-03-20 01:11:49 +0900

    Chore: 초기 세팅

---
+++ src/pages/Main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MainComponents from "../components/MainComponents";
+
+function Main() {
+  return <MainComponents>페이지에서 내려주는 props children</MainComponents>;
+}
+
+export default Main;
