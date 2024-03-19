7a08b266ee90b7def9535e8ecb2b5e2ac866f726
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-03-19 18:14:01 +0900

    Chore: 기초세팅 완료

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': 'off', // 사용되지 않는 변수 경고를 끔
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+  },
+};
---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainComponents from '../components/MainComponents';
+
+function Main() {
+  return (
+    <>
+      <MainComponents />
+    </>
+  );
+}
+
+export default Main;
