233a86aa602ab4ac66b651899b3eb25aa1ea6122
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-03-20 02:00:19 +0900

    Chore: 기초세팅 완료

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,6 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />,
+);
