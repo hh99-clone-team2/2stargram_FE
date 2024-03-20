1b121d0c96efbaf45fe4d2f5acd1b1cf0a4643ce
zook0320 <zook020320@gmail.com>
2024-03-20 14:40:13 +0900

    design-globalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+
 import { Outlet } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Outlet />
     </>
   );
