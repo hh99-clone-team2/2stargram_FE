dc1d1a6e2ce58e61b022978a994b66ed735431d7
zook0320 <zook020320@gmail.com>
2024-03-20 14:51:23 +0900

    globlStyles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -2,11 +2,11 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
 body {
   display: flex;
-  justify-content: center; /* 수평 중앙 정렬 */
-  height: 100vh; /* 화면 전체 높이 */
+  justify-content: center; 
+  height: 100vh; 
 
   max-width: 615px; 
-    margin: 0 auto; /* 가운데 정렬 */
+    margin: 0 auto; 
     box-sizing: border-box; /* padding 값을 포함하여 요소 크기 계산 */
 }
   
